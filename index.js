5161e93182ab2766af8ec2370c0d1d62d1f6631e
Jacob Erisman <jacob.erisman@gmail.com>
2023-08-30 16:43:35 -0400

    Completed Lab 08.30.2023 - JE

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    }
+
+    return gifts;
+}
+
+wrapGifts(gifts);
+
 function writeCards(names, event){
-    let messages = []  
-    for (let i = 0; i < names.length; i++){
+    let messages = []
+    for (let i = 0; i < names.length; i++) {
         messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
     }
-    return messages
+    return messages;
 }
 
 function countDown(number){
-    while (number > -1){
+    while (number >= 0) {
         console.log(number--);
     }
 }
